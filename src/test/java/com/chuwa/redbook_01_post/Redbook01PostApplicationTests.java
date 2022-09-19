301c45752c5c6caef289c14104d420d2d84e7e82
zyang1007 <willzhu0411@gmail.com>
2022-09-18 22:27:08 -0700

    redbook post method

---
+++ src/test/java/com/chuwa/redbook_01_post/Redbook01PostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuwa.redbook_01_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Redbook01PostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
